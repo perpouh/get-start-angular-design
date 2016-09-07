dc8cfc5deee7ab0df915eb5259e3b0161156c292
ぱあぷう <perpouh@gmail.com>
2016-09-07 17:14:02 +0900

    イメージギャラリー（アニメーション追加後）

--- imagegallery/main.js
+++ imagegallery/main.js
@@ -1,8 +1,16 @@
-angular.module('sample', []).controller('IndexController',
+angular.module('sample', ['ngAnimate']).controller('IndexController',
 		[ '$scope', function($scope) {
 			$scope.imgSrc="img/photo1.jpg";
+			$scope.sw=true;
 				
 			$scope.changeMainImage=function(imgSrc){
-				$scope.imgSrc=imgSrc;
+				if($scope.sw){
+					$scope.imgSrc1=imgSrc;
+					$scope.sw=false;
+				}else{
+					$scope.imgSrc2=imgSrc;
+					$scope.sw=true;
+				}
+				
 			};
 		} ]);
