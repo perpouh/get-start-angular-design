f53ac9e1537181fbc6533c72218449dd9b8f6288
ぱあぷう <perpouh@gmail.com>
2016-09-09 09:31:04 +0900

    メニュー切り替えのアニメーションができない(´@ω@｀)

--- imagegallery/main.js
+++ imagegallery/main.js
@@ -2,6 +2,8 @@ angular.module('sample', ['ngAnimate']).controller('IndexController',
 		[ '$scope', function($scope) {
 			$scope.imgSrc="img/photo1.jpg";
 			$scope.sw=true;
+			$scope.margin=0;
+			$scope.myStyle={'margin-left':$scope.margin+'px'};
 				
 			$scope.changeMainImage=function(imgSrc){
 				if($scope.sw){
@@ -13,4 +15,14 @@ angular.module('sample', ['ngAnimate']).controller('IndexController',
 				}
 				
 			};
+			
+			$scope.next=function(){
+				$scope.margin-=300;
+				$scope.myStyle={'margin-left':$scope.margin+'px'};
+			}
+			
+			$scope.prev=function(){
+				$scope.margin+=300;
+				$scope.myStyle={'margin-left':$scope.margin+'px'};
+			}
 		} ]);
