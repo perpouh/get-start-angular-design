feb17cd787a55a9ee020839be9db2a48d81931ec
ぱあぷう <perpouh@gmail.com>
2016-09-12 14:40:34 +0900

    開閉とホバーとなんとかかんとか

--- accordionpanel/main.js
+++ accordionpanel/main.js
@@ -1,4 +1,12 @@
 angular.module('sample', ['ngAnimate']).controller('IndexController',
 		[ '$scope', function($scope) {
 			
+			$scope.onClickHeader=function(num){
+				if($scope.showPanel==num){
+					$scope.showPanel=0;
+				}else{
+					$scope.showPanel=num;
+				}
+			}
+			
 		} ]);
