c2f8cb113d225d258a9a54c7645ea7b265742eb2
ぱあぷう <perpouh@gmail.com>
2016-09-12 17:26:51 +0900

    ﾋﾗﾒｲﾀ(ﾟ∀ﾟ)と思ったら思ったんと違う

--- imagegallery/main.js
+++ imagegallery/main.js
@@ -2,8 +2,8 @@ angular.module('sample', ['ngAnimate']).controller('IndexController',
 		[ '$scope', function($scope) {
 			$scope.imgSrc="img/photo1.jpg";
 			$scope.sw=true;
-			$scope.margin=0;
 			$scope.myStyle={'margin-left':$scope.margin+'px'};
+			$scope.page=1;
 				
 			$scope.changeMainImage=function(imgSrc){
 				if($scope.sw){
@@ -17,12 +17,10 @@ angular.module('sample', ['ngAnimate']).controller('IndexController',
 			};
 			
 			$scope.next=function(){
-				$scope.margin-=300;
-				$scope.myStyle={'margin-left':$scope.margin+'px'};
+				$scope.page++;
 			}
 			
 			$scope.prev=function(){
-				$scope.margin+=300;
-				$scope.myStyle={'margin-left':$scope.margin+'px'};
+				$scope.page--;
 			}
 		} ]);
