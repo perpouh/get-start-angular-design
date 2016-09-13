9ca3f7a041a0f7e33c8fa70939148cce3bfed182
ぱあぷう <perpouh@gmail.com>
2016-09-13 17:32:21 +0900

    ¿½加

--- tooltip/main.js
+++ tooltip/main.js
@@ -1,7 +1,13 @@
 angular.module('sample', []).controller('IndexController',
-		[ '$scope', function($scope) {
+		[ '$scope', '$timeout', function($scope, $timeout) {
 			$scope.showTip=function(event,num){
 				$scope.tip=num;
 				$scope.toolTipStyle={'left':'350px','top':'-8px'};
+			}
+			
+			$scope.hideTip=function(){
+				$timeout(function(){
+					$scope.tip=0;
+				},3000);
 			}
 		} ]);
