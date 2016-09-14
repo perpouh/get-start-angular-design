ce81ca6e67ded70e1ab3f1d4b924a0bbf8f215ae
ぱあぷう <perpouh@gmail.com>
2016-09-14 15:03:29 +0900

    バグフィックス

--- dropdown/main.js
+++ dropdown/main.js
@@ -1,9 +1,3 @@
 angular.module('sample', []).controller('IndexController',
 		[ '$scope', function($scope) {
-			$scope.openMenu=function(num){
-				$scope.menu=num;
-			}
-			$scope.closeMenu=function(){
-				$scope.menu=0;
-			}
 		} ]);
